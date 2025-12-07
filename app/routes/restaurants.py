ee7449843c92c99b039651cc9139623b38e85a15
BennyJetz <benlange439@gmail.com>
2025-12-07 15:51:52 -0600

    Added comments to restaurants.py

--- app/routes/restaurants.py
+++ app/routes/restaurants.py
@@ -4,9 +4,13 @@ from ..models import Restaurants
 
 bp = Blueprint("restaurants", __name__)
 
+#Get all restaurant locations#
 
 @bp.get("/restaurants")
 def get_restaurants():
+    """Returns all restaurant locations."""
+
+    #Convert model objects into simple JSON-friendly dictionaries
     all_restaurants = Restaurants.query.all()
     return jsonify([
         {"id": r.RestaurantID, "name": r.Name, "address": r.Address}
